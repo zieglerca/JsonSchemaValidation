f16bcd4c020491b6cceef89b81ed1df5b5e2378c
Carsten Ziegler <carsten@a-ziegler.de>
2018-09-22 22:41:08 +0200

    support for reference resolution and multiple error output

--- run_test_cases.py
+++ run_test_cases.py
@@ -1,20 +1,25 @@
-from os.path import join, dirname, sep, realpath
+from os.path import join, dirname
 from jsonschema import validate, RefResolver
 import json
-import sys
-import jsonref
+from jsonschema.validators import Draft4Validator
+
+def printErrors(errors):
+    for idx, err in enumerate(errors):
+        print(f"Error {idx}")
+        print(f"  Message: {err.message}")
+        print(f"  Path: {'>'.join(err.path)}")
+        print(f"  Schema Path: {'>'.join(err.schema_path)}")
 
 # load ERM schema
 relative_path = join('schemas', 'erm.json')
 absolute_path = join(dirname(__file__), relative_path)
 with open(absolute_path) as sf:
     schema = json.load(sf)
-
+'''
+# this validator just returns the first error as an exception
 for tc in ['testcase1.json', 'testcase2.json']:
     # load test case
-    with open(tc) as df:
-        data = json.load(df)
-
+    with open(tc) as df: data = json.load(df)
     # validate test case
     try:
         validate(data, schema)
@@ -23,3 +28,23 @@ for tc in ['testcase1.json', 'testcase2.json']:
         print(e)
     else:
         print("completed successfully: {tc}")
+
+# this validator returns all errors in a structured format (better)
+validator = Draft4Validator(schema)
+for tc in ['testcase1.json', 'testcase2.json']:
+    # load test case
+    with open(tc) as df: data = json.load(df)
+    # validate test case
+    errors = list(validator.iter_errors(data))
+    printErrors(errors)
+'''
+# this validator returns all errors in a structured format (better) AND uses a resolver for remote references
+base_uri = "https://raw.githubusercontent.com/zieglerca/JsonSchemaValidation/master/schemas/erm.json"
+resolver = RefResolver(base_uri=base_uri, referrer=schema, cache_remote=True )
+validator = Draft4Validator(schema, resolver=resolver)
+for tc in ['testcase1.json', 'testcase2.json']:
+    # load test case
+    with open(tc) as df: data = json.load(df)
+    # validate test case
+    errors = list(validator.iter_errors(data))
+    printErrors(errors)
